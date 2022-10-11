6a5aa473e3c9afa50fd51529bb973fe043bfb500
Clarathms <clarathaisms12@gmail.com>
2022-10-11 15:05:52 -0300

    Initial Commit

---
+++ AtividadeGit/AtividadeGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtividadeGitApp.swift
+//  AtividadeGit
+//
+//  Created by Clara Thaís Maciel e Silva on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct AtividadeGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
